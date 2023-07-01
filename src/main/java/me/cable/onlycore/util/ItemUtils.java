01c4bd50d6678fe9e150a74d4bc27211fe2bdf91
Cable <cableplays1912@gmail.com>
2023-07-01 16:13:46 +0200

    Updated Item Creation From Config

--- src/main/java/me/cable/onlycore/util/ItemUtils.java
+++ src/main/java/me/cable/onlycore/util/ItemUtils.java
@@ -1,13 +1,19 @@
 package me.cable.onlycore.util;
 
+import me.arcaniax.hdb.api.HeadDatabaseAPI;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -50,23 +56,88 @@ public final class ItemUtils {
         return item;
     }
 
+    @Contract("_, !null -> !null")
+    private static @Nullable Material materialFromName(@Nullable String name, @Nullable Material def) {
+        if (name == null) return def;
+        Material material = Material.getMaterial(name);
+        return (material == null) ? def : material;
+    }
+
     public static @NotNull ItemStack fromCS(
             @NotNull ConfigurationSection cs,
             @Nullable Map<String, String> placeholders
     ) {
-        Material material = null;
+        if (placeholders == null) {
+            placeholders = new HashMap<>();
+        }
+
+        ItemStack item = null;
+
+        /* Material Or Head */
+
         String materialName = cs.getString("material");
 
-        if (materialName != null) {
-            material = Material.getMaterial(materialName);
-        }
-        if (material == null) {
-            material = Material.AIR;
+        if (materialName != null && materialName.startsWith("hdb-")) {
+            if (Bukkit.getPluginManager().isPluginEnabled("HeadDatabase")) {
+                HeadDatabaseAPI headDatabaseAPI = new HeadDatabaseAPI();
+                String headId = materialName.substring("hdb-".length());
+
+                try {
+                    item = headDatabaseAPI.getItemHead(headId);
+                } catch (NullPointerException e) {
+                    // could not find head
+                }
+            }
+            if (item == null) {
+                item = new ItemStack(Material.PLAYER_HEAD);
+            }
+        } else {
+            Material material = materialFromName(materialName, Material.AIR);
+            item = new ItemStack(material);
         }
 
-        String name = cs.getString("name");
-        List<String> lore = cs.getStringList("lore");
-        ItemStack item = item(material, name, lore, placeholders);
+        ItemMeta meta = item.getItemMeta();
+
+        if (meta != null) {
+
+            /* Name */
+
+            String name = cs.getString("name");
+
+            if (name != null) {
+                name = StringUtils.replace(name, placeholders);
+                meta.setDisplayName(FormatUtils.format(name));
+            }
+
+            /* Lore */
+
+            List<String> lore = cs.getStringList("lore");
+            List<String> formattedLore = new ArrayList<>();
+
+            for (String s : lore) {
+                s = StringUtils.replace(s, placeholders);
+                formattedLore.add(FormatUtils.format(s));
+            }
+
+            meta.setLore(formattedLore);
+
+            /* Flags */
+
+            if (cs.getBoolean("flags")) {
+                meta.addItemFlags(ItemFlag.values());
+            }
+
+            /* Glow */
+
+            if (cs.getBoolean("glow")) {
+                meta.addEnchant(Enchantment.DURABILITY, 0, true);
+                meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+            }
+
+            /* Meta */
+
+            item.setItemMeta(meta);
+        }
 
         item.setAmount(cs.getInt("amount", 1));
         return item;
